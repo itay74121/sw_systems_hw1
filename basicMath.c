00bb4d1ec02f4293a5650d80ea833e7d5fdb0f64
itay yosef <itay74121@gmail.com>
2020-11-06 15:33:55 +0200

    second commit, fix bugs and adding commenting

--- basicMath.c
+++ basicMath.c
@@ -1,19 +1,26 @@
 #include "myMath.h"
-
+/*This function adds x+y
+ * */
 float add(float x , float y)
 {
 	return x+y;
 }
+/*This function subtitute x-y
+ * */
 float sub(float x , float y)
 {
 	return x-y;
 }
 
+/*This function multiplies x*y
+ * */
 double mul(double x , int y)
 {
 	return x*y;
 }
 
+/*This function devides x/y
+ * */
 double div(double x, int y)
 {
 	return x/y;
--- power.c
+++ power.c
@@ -1,41 +1,60 @@
 #include "myMath.h"
 
-
+/*This function calculates and exponent like e^x
+ * @ int x - variable x is a whole number 
+ * */
 double Exponent(int x) 
 {
-	double m = 1;
-	if (x==0)
-		return 1;
-	if (x>0)
+	double m = 1; // set m to 1 
+	if (x==0) // if x is 0
+		return m; // return m as it is 
+	if (x>0) // if x is bigger than zero 
 	{
-		for (int i =0; i<x;i++)
-			m*=E;
-		return m;
+		for (int i =0; i<x;i++) // loop x times 
+			m*=E; // multiply by the constant E
+		return m; // return m the answer
 	}
-	else
+	else // otherwise x is a negative int 
 	{
-		for (int i =0; i<x;i++)
-			m*=E;
-		return 1/m;
+		for (int i =0; i<x;i++) // loop x times 
+			m*=E; // multiply by E x times 
+		return 1/m; // do m^-1
 
 	}
 }
+/*Return |x|
+ * */
+int abs(int x )
+{
+	if (x==0)
+		return x;
+	if (x>0)
+		return x;
+	else
+		return -x;
+}
+
+
+/*This funxtion raises x to the power of y 
+ * @ double x  real number x 
+ * @ int y a whole number y
+ * */
 double Power(double x , int y)
 {
-	double m = 1;
-	if (y==0)
-		return 1;
-	if (y>0)
+	double m = 1; // set m to 0
+	if (y==0) // check if y is 0
+		return m; // return m as it is 
+	if (y>0) // if y is positive int 
 	{
-		for (int i =0; i<y;i++)
-			m*=x;
-		return m;
+		for (int i =0; i<y;i++) //loop y times 
+			m*=x; // multiply by x 
+		return m; // return x 
 	}
-	else
-	{
-		for (int i =0; i<y;i++)
-			m*=x;
-		return 1/m;
+	else // otherwise y is a negative int
+	{ 
+		for (int i =0; i<abs(y);i++) // loop y times 
+			m*=x; // multiply by x 
+		return 1/m; // return m^-1
 
 	}
 }
